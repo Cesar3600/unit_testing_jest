c443c90bc27fe19d6576e8f0e8bc4141b99cc739
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-10-22 17:50:00 -0500

    02 implementando pruebas a promesas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -27,3 +27,39 @@ test('Probar un Callback', () => {
 		expect(str).toBe('aloH')
 	})
 })
+
+const reverseString2 = (str) => {
+	return new Promise((resolve, reject) => {
+		if (!str) {
+			reject(Error('Error in promise'))
+		}
+		resolve(str.split('').reverse().join(''))
+	})
+}
+
+test('Probar una promesa', () => {
+	return reverseString2('Hola2').then((string) => {
+		expect(string).toBe('2aloH')
+	})
+})
+
+test('Probar async/await', async () => {
+	const string = await reverseString2('Hola3')
+	expect(string).toBe('3aloH')
+})
+
+afterEach(() => {
+	console.log('despues de cada prueba')
+})
+
+afterAll(() => {
+	console.log('Despues de todas las pruebas')
+})
+
+beforeEach(() => {
+	console.log('antes de cada prueba')
+})
+
+beforeAll(() => {
+	console.log('antes de todas las pruebas')
+})
